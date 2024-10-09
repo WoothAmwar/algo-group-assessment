62803da2da6fcf56a5b023f4530a38dd27dcd488
WoothAmwar <anwar09102005@gmail.com>
2024-10-09 15:54:04 -0500

    Attempted Find Duplicate and Implement Stack, both in Python

--- find-duplicate/option1.py
+++ find-duplicate/option1.py
@@ -2,6 +2,9 @@
 # DO NOT SHARE
 
 from typing import List
+# unittest used for unit testing, not in the findDuplicate function
+import unittest
+
 
 # Implement a function to identify a duplicate integer in an unsorted array
 # of integers. Talk about time/space complexity for each method you implement.
@@ -11,4 +14,53 @@ from typing import List
 # `input` contains all integers in the domain [1, N] at least once
 # `findDuplicate` returns an `int`: the duplicate integer
 def findDuplicate(input: List[int]) -> int:
-    return 0
+    """
+    Description of my two solutions.
+
+    
+    Method 1 has a time complexity of O(N^2) as it traverses the list  at most N times, thus N^2, and space complexity
+      of O(1) since it doesn't use any data structures aside from the input list.
+    Method 2 has a time complexity of O(N) as it only traverses the list once and searching within a set has a time 
+      complexity of O(1) as it behaves like a hashmap. This method has a space complexity of O(N) as it implements a set
+      to save every value the algorithm looks at, which can be at most N items, thus O(N).
+    """
+    # METHOD 1 - O(N^2) time complexity, O(1) space complexity
+    for curr_num_idx in range(len(input)):
+        for all_other_num_idx in range(curr_num_idx+1, len(input)):
+            if input[curr_num_idx] == input[all_other_num_idx]:
+                return input[curr_num_idx]
+    # METHOD 2 - O(N) time complexity, O(N) space complexity
+    found_num = set()
+    for curr_num in input:
+        if curr_num in found_num:
+            return curr_num
+        found_num.add(curr_num)
+    return -1
+
+
+class TestClass(unittest.TestCase):
+    """
+    Used for testing the findDuplicate function
+    """
+    def test_unsorted(self):
+        input_list = [10,2,3,7,8,71,39,6,1,5, 1]
+        duplicate_num = findDuplicate(input_list)    
+        self.assertEqual(duplicate_num, 1, "Unsorted Duplicate function does not work")
+    def test_sorted(self):
+        input_list = [0,1,2,3,4,5,6,7,8,9,10, 5]
+        duplicate_num = findDuplicate(input_list)    
+        self.assertEqual(duplicate_num, 5, "Sorted Duplicate function does not work")
+
+
+def main():
+    sorted_input_list = [0,1,2,3,4,5,6,7,8,9,10, 5]
+    unsorted_input_list = [10,2,3,7,8,71,39,6,1,5, 1]
+    print("Should be 5:", findDuplicate(sorted_input_list))
+    print("Should be 1:", findDuplicate(unsorted_input_list))
+
+
+if __name__ == "__main__":
+    # main() should be called to see print statements in terminal
+    # unittest.main() should be called to see the results of the unit tests, which was what was provided in the repo
+    # main()
+    unittest.main()
--- implement-stack/option2.py
+++ implement-stack/option2.py
@@ -4,10 +4,60 @@
 # 2. Implement a growable integer stack (without using container libraries like vector, list, etc.)
 #    that satisfies the following requirements:
 
+class Node:
+    def __init__(self, data, before_node=None, after_node=None):
+        self.data = data
+        self.next:Node = after_node
+        self.before:Node = before_node
+
 # `push` adds a given value to the top
 # `pop`  returns and removes the value at the top
 # `peek` returns the value at the top
 # `size` returns the count of values
-
 class IntStack:
-    pass
+    def __init__(self):
+        # Intializing values for head and end of stack
+        self.end = None
+        self.head = Node(None, after_node=self.end)
+
+    def push(self, value):
+        curr_node = self.head
+        while curr_node.next != None:
+            curr_node = curr_node.next
+        new_node = Node(value, before_node=curr_node)
+        curr_node.next = new_node
+        self.end = new_node
+
+    
+    def pop(self):
+        pop_val = self.end.data
+        if self.end.before == None:
+            return None
+        self.end.before.next = None
+        self.end = self.end.before
+        return pop_val
+
+
+    def peek(self):
+        return self.end.data
+    
+    def size(self):
+        curr_node = self.head.next
+        stack_len = 0
+        while curr_node != None:
+            stack_len += 1
+            curr_node = curr_node.next
+        return stack_len
+
+    # def return_whole_stack(self):
+    #     """
+    #     Used to test the program, has no effect on push, pop, peek, and size functions
+    #     """
+    #     curr_node = self.head.next
+    #     items = []
+    #     while curr_node != None:
+    #         items.append(curr_node.data)
+    #         curr_node = curr_node.next
+    #     return items
+
+    
